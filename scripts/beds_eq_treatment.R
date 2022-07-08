c6911bfee8a97797c6a5830c72db710bfd75c88b
TumeloS <tumelo@aims.ac.za>
2022-07-08 13:45:07 +0200

    Modified script. beds_eq_treat.R

--- scripts/beds_eq_treatment.R
+++ scripts/beds_eq_treatment.R
@@ -17,9 +17,11 @@ treat_cm <- cum_mortality_df %>%
 
 cum_mortality_df %>% 
   filter(round(ps, 2) == round(base_ps, 2), du == base_du) %>% 
-  mutate(tmp = abs(cm - as.numeric(treat_cm))) 
+  mutate(tmp = cm - as.numeric(treat_cm)) %>%
+  filter(abs(tmp) < 5)
 
-cum_mortality_df %>% 
-       mutate(tmp = abs(cm - as.numeric(treat_cm))) %>% 
-       group_by(ps, du) %>% 
-       filter(tmp == min(tmp)) %>% View()
+ bed_scen <- cum_mortality_df %>% 
+  filter(round(ps, 2) == round(base_ps, 2), du == base_du) %>% 
+  mutate(tmp = cm - as.numeric(treat_cm)) %>%
+  filter(abs(tmp) == min(abs(tmp)))
+  
