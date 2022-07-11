17272ff87878c4111a867b568d2c1af5c2976fa1
TumeloS <tumelo@aims.ac.za>
2022-07-11 16:22:28 +0200

    cost

--- scripts/cost_benefit.R
+++ scripts/cost_benefit.R
@@ -1,39 +1,26 @@
 bed_scen <- readRDS("./data/cost_analysis.rds")
+
 load("output/sars_cov_2_outputs.RData")
-baseline_output %>% select(Cur_addm, cum_add_days)
+
+
+my_data <- baseline_output %>% select(Cur_addm, cum_add_days) %>% 
+  mutate(delta.B = replicate(nrow(baseline_output), delta.B)) %>% 
+  mutate(Cab = seq(0, 365, 1))
+
 # Cost of treatment
 
 delta.B <- bed_scen$cc - 20
 
-Cost_tret <- function(A_t, C_a, cum_add){
-  with(parms, {
-    
-    A_t*C_a + C_t*cum_add 
-    
-  })
-}
-
-
-## Cost of beds
-
-Cost_beds <- function(A_b, C_a, delta.B){
-  with(parms, {
-    
-    A_b*C_a + delta.B*C_b
-    
-  })
-}
-
-inp <- c(A_b = 20,
-         A_t = 20,
-         delta.B = 28-20)
-
 # To have a net cost benefit we assume Cost of treatment must be less than cost of beds.
-# That is cost_tret > cost_beds
+# That is cost_tret > cost_beds. 
+# Let C_b/C_a = variable Cba and 
 
-C_t/C_a <- function(C_b/C_a, A_b, A_t, delta.B) {
+Cost_ba <- function(Cab, A_b, A_t, delta.B) {
    with(parms, {
-     (A_b + A_t + delta.B * (C_b / C_a)) / (A_t)
+     (A_b - A_t + delta.B * Cab) / H
    })
- }
+}
 
+my_data[2, ]
+
+Cost_ba(Cab = my_data[2,1], A_t = 20,  delta.B = my_data$delta.B)
